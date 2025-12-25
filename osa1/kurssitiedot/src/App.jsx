8e9cf044e29dc355c44fdda7105a769cf4373260
vhiltzu <ville.c.hamalainen@helsinki.fi>
2025-12-25 23:42:10 +0200

    Refactor kurssitiedot part 1

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -2,6 +2,40 @@ import Header from "./Header"
 import Content from "./Content"
 import Total from "./Total"
 
+const Header = (props) => {
+  return <h1>{props.course}</h1>;
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      {props.parts.map((part) => (
+        <Part key={part.name} part={part.name} exercises={part.exercises} />
+      ))}
+    </div>
+  );
+};
+
+const Part = ({ part, exercises }) => {
+  return (
+    <p>
+      {part} {exercises}
+    </p>
+  );
+};
+
+const Total = (props) => {
+  const sumExercises = (parts) => {
+    let total = 0;
+    for (const part of parts) {
+      total += part.exercises;
+    }
+    return total;
+  };
+
+  return <p>Number of exercises {sumExercises(props.parts)}</p>;
+};
+
 const App = () => {
   const course = {
     name: 'Half Stack application development',
