f30e0d149e70ca32af700126581b9d85fd7c9a1f
vhiltzu <ville.c.hamalainen@helsinki.fi>
2025-12-25 23:23:55 +0200

    #17 Completed exercise 2.1

--- osa2/kurssitiedot/src/App.jsx
+++ osa2/kurssitiedot/src/App.jsx
@@ -1,34 +1,33 @@
-import Header from "./Header"
-import Content from "./Content"
-import Total from "./Total"
+import Course from "./Course";
 
 const App = () => {
   const course = {
-    name: 'Half Stack application development',
+    name: "Half Stack application development",
+    id: 1,
     parts: [
       {
-        name: 'Fundamentals of React',
-        exercises: 10
+        name: "Fundamentals of React",
+        id: 1,
+        exercises: 10,
       },
       {
-        name: 'Using props to pass data',
-        exercises: 7
+        name: "Using props to pass data",
+        id: 2,
+        exercises: 7,
       },
       {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
-
+        name: "State of a component",
+        id: 3,
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <Course course={course} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
