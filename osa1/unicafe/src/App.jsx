6ff1ddf44c6f315213ac5dc2f7cd8496334bda37
vhiltzu <ville.c.hamalainen@helsinki.fi>
2025-12-25 23:40:19 +0200

    Refactor unicafe

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -2,6 +2,43 @@ import { useState } from 'react'
 import Statistics from "./Statistics";
 import Button from "./Button";
 
+const Button = (props) => {
+  return <button onClick={props.onClick}>{props.text}</button>;
+};
+
+const StatisticLine = (props) => {
+  return (
+    <tr>
+      <td>{props.text}</td>
+      <td>{props.value}</td>
+    </tr>
+  );
+};
+
+const Statistics = (props) => {
+  const all = props.good + props.bad * -1; // good = 1, neutral = 0, bad = -1
+  const avg = all / (props.good + props.neutral + props.bad);
+  const positive =
+    (props.good / (props.good + props.neutral + props.bad)) * 100;
+
+  if (all === 0) {
+    return <div>No feedback given</div>;
+  }
+
+  return (
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={props.good} />
+        <StatisticLine text="neutral" value={props.neutral} />
+        <StatisticLine text="bad" value={props.bad} />
+        <StatisticLine text="all" value={all} />
+        <StatisticLine text="average" value={avg} />
+        <StatisticLine text="positive" value={positive + " %"} />
+      </tbody>
+    </table>
+  );
+};
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
