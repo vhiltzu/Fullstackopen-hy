c9f1610828d8884188bbf0292541d9ead29921f7
vhiltzu <ville.c.hamalainen@helsinki.fi>
2025-12-25 23:39:18 +0200

    #21 Completed exercise 2.5

--- osa2/kurssitiedot/src/Course.jsx
+++ osa2/kurssitiedot/src/Course.jsx
@@ -1,6 +1,6 @@
-import Header from "./Header";
-import Content from "./Content";
-import Total from "./Total";
+import Header from "../Header";
+import Content from "./Course/Content";
+import Total from "./Course/Total";
 
 const Course = (props) => {
   return (
@@ -13,3 +13,42 @@ const Course = (props) => {
 };
 
 export default Course;
+
+const Header = (props) => {
+  return <h1>{props.course}</h1>;
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      {props.parts.map((part) => (
+        <Part key={part.id} part={part.name} exercises={part.exercises} />
+      ))}
+    </div>
+  );
+};
+
+const Part = ({ part, exercises }) => {
+  return (
+    <p>
+      {part} {exercises}
+    </p>
+  );
+};
+
+const Total = (props) => {
+  // Calculate the total number of exercises
+  const sum = (sum, part) => {
+    return sum + part.exercises;
+  };
+
+  const total = props.parts.reduce(sum, 0);
+
+  return (
+    <p>
+      <b>total of {total} exercises</b>
+    </p>
+  );
+};
+
+export { Header, Content, Part, Total };
