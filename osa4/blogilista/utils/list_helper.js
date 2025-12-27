d1641edb7adbfa8345ae55087ecb7ec1a094bffe
vhiltzu <ville.c.hamalainen@helsinki.fi>
2025-12-28 01:00:58 +0200

    #42 Completed exercise 4.5

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -3,12 +3,22 @@ const dummy = (_blogs) => {
 };
 
 const totalLikes = (blogs) => {
+  // Sum the likes of all blogs
   const sumLikes = (sum, blog) => sum + blog.likes;
 
   return blogs.reduce(sumLikes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  // Find the blog with the most likes
+  // That is the favorite blog
+  const mostLiked = (max, blog) => (blog.likes > max.likes ? blog : max);
+
+  return blogs.reduce(mostLiked, blogs[0]);
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
