fb5df014a36fa6d2b2a7261046d12176bf7139ba
vhiltzu <ville.c.hamalainen@helsinki.fi>
2025-12-28 15:18:32 +0200

    #74 Completed exercise 5.14

--- osa5/bloglist-frontend/src/components/Blog.test.jsx
+++ osa5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Blog from "./Blog";
 
 test("renders only title and author", () => {
@@ -20,4 +21,24 @@ test("renders only title and author", () => {
   expect(urlElement).toBeNull();
   const likesElement = screen.queryByText("likes 5");
   expect(likesElement).toBeNull();
+});
+
+test("renders url and likes when view button is clicked", async () => {
+  const blog = {
+    title: "Component testing is done with react-testing-library",
+    author: "Testing Library",
+    url: "http://localhost:3000",
+    likes: 5,
+  };
+
+  render(<Blog blog={blog} />);
+  const events = userEvent.setup();
+
+  const button = screen.getByText("view");
+  await events.click(button);
+
+  const urlElement = screen.getByText("http://localhost:3000");
+  expect(urlElement).toBeDefined();
+  const likesElement = screen.getByText("likes 5");
+  expect(likesElement).toBeDefined();
 });
