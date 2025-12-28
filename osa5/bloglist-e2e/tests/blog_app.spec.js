18a5b373c2bab56085878935085889c840f2446d
vhiltzu <ville.c.hamalainen@helsinki.fi>
2025-12-28 22:29:05 +0200

    #79 Completed exercise 5.19

--- osa5/bloglist-e2e/tests/blog_app.spec.js
+++ osa5/bloglist-e2e/tests/blog_app.spec.js
@@ -38,4 +38,37 @@ describe("Blog app", () => {
       await expect(page.getByText("wrong username or password")).toBeVisible();
     });
   });
+
+  describe("When logged in", () => {
+    beforeEach(async ({ page }) => {
+      await page.goto("http://localhost:5173");
+
+      await page.getByLabel("username").fill("mluukkai");
+      await page.getByLabel("password").fill("salainen");
+      await page.getByRole("button", { name: "login" }).click();
+
+      // Verify that login was successful
+      await expect(page.getByText("Matti Luukkainen logged in")).toBeVisible();
+    });
+
+    test("a new blog can be created", async ({ page }) => {
+      await page.getByRole("button", { name: "create new blog" }).click();
+      await page.getByLabel("title:").fill("E2E testing with Playwright");
+      await page.getByLabel("author:").fill("Test Author");
+      await page.getByLabel("url:").fill("http://example.com");
+      await page.getByRole("button", { name: "create" }).click();
+
+      // Verify that the notification appears
+      await expect(
+        page.getByText(
+          'A new blog "E2E testing with Playwright" by Test Author added'
+        )
+      ).toBeVisible();
+
+      // Verify that the new blog appears in the blog list
+      await expect(
+        page.getByText("E2E testing with Playwright Test Author")
+      ).toBeVisible();
+    });
+  });
 });
