1da38f5293ceeb38c46aca0ed09bed26a98687af
vhiltzu <ville.c.hamalainen@helsinki.fi>
2025-12-28 14:16:58 +0200

    #63 Completed exercise 5.3

--- osa5/bloglist-frontend/src/App.jsx
+++ osa5/bloglist-frontend/src/App.jsx
@@ -8,14 +8,21 @@ const App = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [user, setUser] = useState(null);
+  const [newBlogTitle, setNewBlogTitle] = useState("");
+  const [newBlogAuthor, setNewBlogAuthor] = useState("");
+  const [newBlogUrl, setNewBlogUrl] = useState("");
 
   useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
     const loggedUserJSON = window.localStorage.getItem("loggedBlogAppUser");
-    if (loggedUserJSON) {
-      const user = JSON.parse(loggedUserJSON);
-      setUser(user);
+
+    if (!loggedUserJSON) {
+      return;
     }
+
+    const user = JSON.parse(loggedUserJSON);
+    setUser(user);
+    blogService.setToken(user.token);
   }, []);
 
   const handleLogin = (event) => {
@@ -26,6 +33,7 @@ const App = () => {
       .then((user) => {
         localStorage.setItem("loggedBlogAppUser", JSON.stringify(user));
         setUser(user);
+        blogService.setToken(user.token);
 
         setUsername("");
         setPassword("");
@@ -37,7 +45,24 @@ const App = () => {
 
   const handleLogout = () => {
     setUser(null);
+    blogService.setToken(null);
     localStorage.removeItem("loggedBlogAppUser");
+  };
+
+  const handleNewBlogCreation = (event) => {
+    event.preventDefault();
+    const blogObject = {
+      title: newBlogTitle,
+      author: newBlogAuthor,
+      url: newBlogUrl,
+    };
+
+    blogService.create(blogObject).then((returnedBlog) => {
+      setBlogs(blogs.concat(returnedBlog));
+      setNewBlogTitle("");
+      setNewBlogAuthor("");
+      setNewBlogUrl("");
+    });
   };
 
   // Show login form if user is not logged in
@@ -75,15 +100,43 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <div>
+      <p>
         {user.name} logged in{" "}
         <button type="button" onClick={handleLogout}>
           logout
         </button>
-      </div>
+      </p>
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
       ))}
+      <h2>Create a new blog</h2>
+      <form onSubmit={handleNewBlogCreation}>
+        <div>
+          title:
+          <input
+            type="text"
+            value={newBlogTitle}
+            onChange={({ target }) => setNewBlogTitle(target.value)}
+          />
+        </div>
+        <div>
+          author:
+          <input
+            type="text"
+            value={newBlogAuthor}
+            onChange={({ target }) => setNewBlogAuthor(target.value)}
+          />
+        </div>
+        <div>
+          url:
+          <input
+            type="text"
+            value={newBlogUrl}
+            onChange={({ target }) => setNewBlogUrl(target.value)}
+          />
+        </div>
+        <button type="submit">create</button>
+      </form>
     </div>
   );
 };
--- osa5/bloglist-frontend/src/services/blogs.js
+++ osa5/bloglist-frontend/src/services/blogs.js
@@ -1,9 +1,23 @@
 import axios from "axios";
 const baseUrl = "http://localhost:3003/api/blogs";
 
+let token = null;
+
+const setToken = (newToken) => {
+  token = `Bearer ${newToken}`;
+};
+
 const getAll = () => {
   const request = axios.get(baseUrl);
   return request.then((response) => response.data);
 };
 
-export default { getAll };
+const create = (newObject) => {
+  const config = {
+    headers: { Authorization: token },
+  };
+  const request = axios.post(baseUrl, newObject, config);
+  return request.then((response) => response.data);
+};
+
+export default { getAll, create, setToken };
