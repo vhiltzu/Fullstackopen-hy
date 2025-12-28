08639a2685c89033f7faa7cd1d74c6602fee32f4
vhiltzu <ville.c.hamalainen@helsinki.fi>
2025-12-28 14:02:51 +0200

    #61 Completed exercise 5.1

---
+++ osa5/bloglist-frontend/src/App.jsx
@@ -0,0 +1,73 @@
+import { useState, useEffect } from "react";
+import Blog from "./components/Blog";
+import blogService from "./services/blogs";
+import loginService from "./services/login";
+
+const App = () => {
+  const [blogs, setBlogs] = useState([]);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+
+    loginService
+      .login(username, password)
+      .then((user) => {
+        setUser({ username: user.username, name: user.name });
+        setUsername("");
+        setPassword("");
+      })
+      .catch(() => {
+        alert("Wrong credentials");
+      });
+  };
+
+  // Show login form if user is not logged in
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        <form onSubmit={handleLogin}>
+          <div>
+            <label>
+              username
+              <input
+                type="text"
+                value={username}
+                onChange={({ target }) => setUsername(target.value)}
+              />
+            </label>
+          </div>
+          <div>
+            <label>
+              password
+              <input
+                type="password"
+                value={password}
+                onChange={({ target }) => setPassword(target.value)}
+              />
+            </label>
+          </div>
+          <button type="submit">login</button>
+        </form>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h2>blogs</h2>
+      {blogs.map((blog) => (
+        <Blog key={blog.id} blog={blog} />
+      ))}
+    </div>
+  );
+};
+
+export default App;
---
+++ osa5/bloglist-frontend/src/services/login.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+const baseUrl = "http://localhost:3003/api/login";
+
+const login = (username, password) => {
+  const request = axios.post(baseUrl, { username, password });
+  return request.then((response) => response.data);
+};
+
+export default { login };
