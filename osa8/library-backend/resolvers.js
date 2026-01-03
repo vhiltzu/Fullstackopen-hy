e2c6d82c71a54e2396fd9ef806e41d2f12339a36
Ville Hämäläinen <ville.c.hamalainen@helsinki.fi>
2026-01-03 16:45:40 +0200

    #152 Completed exercise 8.13

---
+++ osa8/library-backend/resolvers.js
@@ -0,0 +1,63 @@
+const { GraphQLError } = require("graphql");
+const { ApolloServerErrorCode } = require("@apollo/server/errors");
+const { v1: uuid } = require("uuid");
+const Author = require("./models/author");
+const Book = require("./models/book");
+
+const resolvers = {
+  Query: {
+    bookCount: async () => Book.collection.countDocuments(),
+    authorCount: async () => Author.collection.countDocuments(),
+    allBooks: async (_, args) => {
+      return await Book.find({
+        ...(args.author && { author: args.author }),
+        ...(args.genre && { genres: args.genre }),
+      });
+    },
+    allAuthors: async () => {
+      return await Author.find({}).populate("bookCount");
+    },
+  },
+  Mutation: {
+    addBook: async (_, args) => {
+      const bookExists = await Book.findOne({
+        title: args.title,
+        author: args.author,
+      });
+
+      if (bookExists) {
+        throw new GraphQLError(
+          `Title and author must be unique: ${args.title}`,
+          {
+            extensions: {
+              code: ApolloServerErrorCode.BAD_USER_INPUT,
+              invalidArgs: [args.title, args.author],
+            },
+          }
+        );
+      }
+
+      let author = await Author.findOne({ name: args.author });
+
+      if (!author) {
+        author = new Author({ name: args.author, id: uuid() });
+        await author.save();
+      }
+
+      const newBook = new Book({ ...args, id: uuid(), author: author._id });
+      return await newBook.save();
+    },
+    editAuthor: async (_, args) => {
+      const author = await Author.findOne({ name: args.name });
+
+      if (!author) {
+        return null;
+      }
+
+      author.born = args.setBornTo;
+      return await author.save();
+    },
+  },
+};
+
+module.exports = resolvers;
