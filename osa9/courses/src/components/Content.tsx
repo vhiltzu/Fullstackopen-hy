2f26cbcbec657cc9c19bb374019befcb2b887562
Ville Hämäläinen <ville.c.hamalainen@helsinki.fi>
2026-01-05 02:39:48 +0200

    #181 Completed exercise 9.16

--- osa9/courses/src/components/Content.tsx
+++ osa9/courses/src/components/Content.tsx
@@ -1,30 +1,110 @@
-interface CoursePartProps {
-  name: string;
-  exerciseCount: number;
-}
+import type {
+  CoursePart,
+  CoursePartBackground,
+  CoursePartBasic,
+  CoursePartGroup,
+  CoursePartSpecial,
+} from "../types";
 
 interface ContentProps {
-  parts: CoursePartProps[];
+  parts: CoursePart[];
 }
+
+const assertNever = (value: never): never => {
+  throw new Error(
+    `Unhandled discriminated union member: ${JSON.stringify(value)}`
+  );
+};
 
 const Content = ({ parts }: ContentProps) => {
   return (
     <div>
-      {parts.map((part) => (
-        <Part
-          key={part.name}
-          name={part.name}
-          exerciseCount={part.exerciseCount}
-        />
-      ))}
+      {parts.map((part) => {
+        switch (part.kind) {
+          case "basic":
+            return <BasicPart key={part.name} part={part} />;
+          case "group":
+            return <GroupPart key={part.name} part={part} />;
+          case "background":
+            return <BackgroundPart key={part.name} part={part} />;
+          case "special":
+            return <SpecialPart key={part.name} part={part} />;
+          default:
+            return assertNever(part);
+        }
+      })}
     </div>
   );
 };
 
-const Part = ({ name, exerciseCount }: CoursePartProps) => {
+interface BasicPartProps {
+  part: CoursePartBasic;
+}
+const BasicPart = ({ part }: BasicPartProps) => {
   return (
     <p>
-      {name} {exerciseCount}
+      <div>
+        <b>
+          {part.name} {part.exerciseCount}
+        </b>
+      </div>
+      <div>
+        <i>{part.description}</i>
+      </div>
+    </p>
+  );
+};
+
+interface GroupPartProps {
+  part: CoursePartGroup;
+}
+const GroupPart = ({ part }: GroupPartProps) => {
+  return (
+    <p>
+      <div>
+        <b>
+          {part.name} {part.exerciseCount}
+        </b>
+      </div>
+      <div>project exercises: {part.groupProjectCount}</div>
+    </p>
+  );
+};
+
+interface BackgroundPartProps {
+  part: CoursePartBackground;
+}
+const BackgroundPart = ({ part }: BackgroundPartProps) => {
+  return (
+    <p>
+      <div>
+        <b>
+          {part.name} {part.exerciseCount}
+        </b>
+      </div>
+      <div>
+        <i>{part.description}</i>
+      </div>
+      <div>background material: {part.backgroundMaterial}</div>
+    </p>
+  );
+};
+
+interface SpecialPartProps {
+  part: CoursePartSpecial;
+}
+const SpecialPart = ({ part }: SpecialPartProps) => {
+  return (
+    <p>
+      <div>
+        <b>
+          {part.name} {part.exerciseCount}
+        </b>
+      </div>
+      <div>
+        <i>{part.description}</i>
+      </div>
+      <div>required skills: {part.requirements.join(", ")}</div>
     </p>
   );
 };
